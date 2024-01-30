563e8967fcfd0cffe4286bbeb0b5286672d3a934
Louis DEVIE <louisdevie.contact@gmail.com>
2024-01-30 13:56:39 +0100

    Création du projet

---
+++ CBReader/src/main/java/fr/iut/ld/cbreader/CbReaderApplication.java
@@ -0,0 +1,13 @@
+package fr.iut.ld.cbreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbReaderApplication.class, args);
+	}
+
+}
---
+++ CBReader/src/test/java/fr/iut/ld/cbreader/CbReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.iut.ld.cbreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
